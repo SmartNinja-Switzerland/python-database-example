13896eb287f3e33907bda8eab91dc76e303b367c
Peter Bittner <peter@painless.software>
2019-12-09 20:48:05 +0100

    Show person details, edit details, update person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template
 
-from addressbook import initialize_database, add_database_records, read_database_records
+from addressbook import initialize_database, add_database_records, read_database_records, db, Person
 
 app = Flask(__name__)
 
@@ -8,6 +8,27 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html", contacts=read_database_records())
+
+
+@app.route("/person/<person_id>", methods=["GET"])
+def details(person_id):
+    person = db.query(Person).get(int(person_id))
+    return render_template("details.html", person=person)
+
+
+@app.route("/person/<person_id>", methods=["POST"])
+def update(person_id):
+    person = db.query(Person).get(int(person_id))
+    # TODO: update in the database
+    print(f"TODO: update {person.firstname} {person.lastname} in the database")
+    # Easy solution: show all contacts after updating
+    return index()
+
+
+@app.route("/edit/<person_id>")
+def edit(person_id):
+    person = db.query(Person).get(int(person_id))
+    return render_template("edit.html", person=person)
 
 
 @app.route("/json")
