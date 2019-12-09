131d5860c75b0360b046c268e5818a1a9700f597
Peter Bittner <peter@painless.software>
2019-12-09 21:13:43 +0100

    Move database code to addressbook module

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template
 
-from addressbook import initialize_database, add_database_records, read_database_records, db, Person
+from addressbook import initialize_database, add_database_records, read_database_records, get_database_item
 
 app = Flask(__name__)
 
@@ -10,25 +10,23 @@ def index():
     return render_template("index.html", contacts=read_database_records())
 
 
-@app.route("/person/<person_id>", methods=["GET"])
+@app.route("/person/<int:person_id>", methods=["GET"])
 def details(person_id):
-    person = db.query(Person).get(int(person_id))
-    return render_template("details.html", person=person)
+    return render_template("details.html", person=get_database_item(person_id))
 
 
-@app.route("/person/<person_id>", methods=["POST"])
+@app.route("/person/<int:person_id>", methods=["POST"])
 def update(person_id):
-    person = db.query(Person).get(int(person_id))
+    person = get_database_item(person_id)
     # TODO: update in the database
     print(f"TODO: update {person.firstname} {person.lastname} in the database")
     # Easy solution: show all contacts after updating
     return index()
 
 
-@app.route("/edit/<person_id>")
+@app.route("/edit/<int:person_id>")
 def edit(person_id):
-    person = db.query(Person).get(int(person_id))
-    return render_template("edit.html", person=person)
+    return render_template("edit.html", person=get_database_item(person_id))
 
 
 @app.route("/json")
